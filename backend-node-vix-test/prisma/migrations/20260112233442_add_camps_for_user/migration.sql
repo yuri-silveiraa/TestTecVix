6c57fd5fe4356a6fc5bbae7d9ec18e4022bc93a5
Yuri <yuri.pires.silveira@gmail.com>
2026-01-12 20:57:24 -0300

    refactor: adicionando campos fullName, userPhoneNumber, department, field and contractDate no User

---
+++ backend-node-vix-test/prisma/migrations/20260112233442_add_camps_for_user/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE `user` ADD COLUMN `contractDate` VARCHAR(191) NULL,
+    ADD COLUMN `department` VARCHAR(191) NULL,
+    ADD COLUMN `field` VARCHAR(191) NULL,
+    ADD COLUMN `fullName` VARCHAR(191) NULL,
+    ADD COLUMN `userPhoneNumber` VARCHAR(191) NULL;
