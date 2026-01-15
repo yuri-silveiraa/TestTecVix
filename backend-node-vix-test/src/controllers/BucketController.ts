8a461f52edc62fc62d264c5356079d90eafde5cb
Yuri <yuri.pires.silveira@gmail.com>
2026-01-14 21:58:54 -0300

    refactor: adicionando toString() ao objectName

--- backend-node-vix-test/src/controllers/BucketController.ts
+++ backend-node-vix-test/src/controllers/BucketController.ts
@@ -12,13 +12,13 @@ export class BucketController {
     res: Response,
   ) {
     const { objectName } = req.params;
-    const filePath = path.join(__dirname, "..", "..", "uploads", objectName);
+    const filePath = path.join(__dirname, "..", "..", "uploads", objectName.toString());
     return res.sendFile(filePath);
   }
 
   async getFileByObjectName(req: CustomRequest<unknown>, res: Response) {
     const { objectName } = req.params;
-    const response = await this.bucketService.renewPresignedUrl(objectName);
+    const response = await this.bucketService.renewPresignedUrl(objectName.toString());
     return res.status(STATUS_CODE.OK).json({ url: response });
   }
 
