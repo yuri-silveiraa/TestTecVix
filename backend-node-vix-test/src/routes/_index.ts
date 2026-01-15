cf02ba3ac460311ee319ba03f68c4692bc57effd
Yuri <yuri.pires.silveira@gmail.com>
2026-01-14 22:02:06 -0300

    feat: criando rotas para o CRUD de usuarios

--- backend-node-vix-test/src/routes/_index.ts
+++ backend-node-vix-test/src/routes/_index.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { brandMasterRoutes } from "./brandMaster.routes";
 import { vMRoutes } from "./vM.routes";
 import { uploadsRoutes } from "./uploads.routes";
+import { userRoutes } from "./user.routes";
 
 export const routes = Router();
 
 routes.use(uploadsRoutes);
 routes.use(brandMasterRoutes);
 routes.use(vMRoutes);
+routes.use(userRoutes);
---
+++ backend-node-vix-test/src/routes/user.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { API_VERSION, ROOT_PATH } from "../constants/basePathRoutes";
+import { UserController } from "../controllers/UserController";
+
+const BASE_PATH = API_VERSION.V1 + ROOT_PATH.USERS;
+
+const userRoutes = Router();
+const userController = new UserController();
+
+userRoutes.post(`${BASE_PATH}`,  (req, res) => { userController.create(req, res); });
+userRoutes.get(`${BASE_PATH}`, (req, res) => { userController.list(req, res); });
+userRoutes.get(`${BASE_PATH}/:idUser`, (req, res) => { userController.getById(req, res); });
+userRoutes.put(`${BASE_PATH}/:idUser`, (req, res) => { userController.update(req, res); });
+userRoutes.delete(`${BASE_PATH}/:idUser`, (req, res) => { userController.delete(req, res); }); 
+
+export { userRoutes };
